9fc0237b4bed7ace0984c2a6441025f91b4c69b3
Willemien van Tonder <willesvz@gmail.com>
2025-03-12 21:44:32 +0200

    WIP

--- src/app/shared/services/payflex.service.ts
+++ src/app/shared/services/payflex.service.ts
@@ -5,42 +5,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PayflexService {
-  private readonly authUrl = 'https://auth-uat.payflex.co.za/auth/merchant';
-  private readonly createOrderUrl = 'https://api.uat.payflex.co.za/order';
-
-  private readonly clientId = 's24rhTxLINDzBaOUURG8AeFR8XdnokhY';
-  private readonly clientSecret = 'aXuHkiSTqG0SadGjWJk7j0jQ47urx4HrgeOeQWWfeQ4WVwgXy3WmhO8mg5dtYWJU';
+  private readonly apiGatewayUrl = 'https://1o6ucfn6n2.execute-api.us-east-1.amazonaws.com/prod1/create-order';
 
   constructor(private readonly http: HttpClient) {}
 
-  // Step 1: Get Access Token
-  async getAccessToken(): Promise<string> {
-    const body = {
-      client_id: this.clientId,
-      client_secret: this.clientSecret,
-      audience: 'https://auth-production.payflex.co.za',
-      grant_type: 'client_credentials'
-    };
-
-    try {
-      const response = await this.http.post<{ access_token: string }>(this.authUrl, body).toPromise();
-      return response.access_token;
-    } catch (error) {
-      console.error('Error getting token:', error);
-      throw error;
-    }
-  }
-
-  // Step 2: Create Order
   async createOrder(amount: number, formData: any) {
-     const token = await this.getAccessToken();
-
     const orderData = {
       amount,
       consumer: {
         phoneNumber: formData.cellphone,
         givenNames: formData.firstName,
-        surname:  formData.lastName,
+        surname: formData.lastName,
         email: formData.userEmail
       },
       description: 'Hungr Subscription',
@@ -56,7 +31,7 @@ export class PayflexService {
       merchant: {
         redirectConfirmUrl: 'https://main.d9ek0iheftizq.amplifyapp.com/verify-email-address',
         redirectCancelUrl: 'https://main.d9ek0iheftizq.amplifyapp.com/cancel-payment',
-        statusCallbackUrl: "https://merchantsite.com/callback"
+        statusCallbackUrl: 'https://merchantsite.com/callback'
       },
       merchantReference: 'Hungr Subscription' + formData.userEmail,
       taxAmount: 0,
@@ -64,16 +39,14 @@ export class PayflexService {
     };
 
     try {
-      const response: any = await this.http.post(this.createOrderUrl, orderData, {
-        headers: { Authorization: `Bearer ${token}` }
-      }).toPromise();
+      const response: any = await this.http.post(this.apiGatewayUrl, orderData).toPromise();
 
       if (response?.redirectUrl) {
-        window.location.href = response.redirectUrl; 
+        window.location.href = response.redirectUrl;
       }
     } catch (error) {
       console.error('Error creating order:', error);
       throw error;
-    } 
+    }
   }
 }
