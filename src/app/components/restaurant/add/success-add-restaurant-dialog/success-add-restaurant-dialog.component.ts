9855a1138f0f173b2a23fbfabdd8b3f8fbfa5a5b
Willemien van Tonder <willesvz@gmail.com>
2025-03-18 22:44:02 +0200

    WIP

--- src/app/components/restaurant/add/success-add-restaurant-dialog/success-add-restaurant-dialog.component.ts
+++ src/app/components/restaurant/add/success-add-restaurant-dialog/success-add-restaurant-dialog.component.ts
@@ -4,11 +4,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-success-add-restaurant-dialog',
   templateUrl: './success-add-restaurant-dialog.component.html',
-  styleUrl: './success-add-restaurant-dialog.component.scss'
+  styleUrl: './success-add-restaurant-dialog.component.scss',
 })
 export class SuccessAddRestaurantDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { title: string, message: string },
-  private dialogRef: MatDialogRef<SuccessAddRestaurantDialogComponent>) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string },
+    private readonly dialogRef: MatDialogRef<SuccessAddRestaurantDialogComponent>
+  ) {}
 
   closeDialog() {
     this.dialogRef.close();
