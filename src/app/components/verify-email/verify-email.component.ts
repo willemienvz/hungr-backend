45843298eabd5901932db4b870520849f1d1943a
Willemien van Tonder <willesvz@gmail.com>
2025-05-13 14:43:12 +0200

    test

--- src/app/components/verify-email/verify-email.component.ts
+++ src/app/components/verify-email/verify-email.component.ts
@@ -1,78 +1,117 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "../../shared/services/auth.service";
+import { AuthService } from '../../shared/services/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { EmailService } from '../../shared/services/email.service';
 import { AngularFireFunctions } from '@angular/fire/compat/functions';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.scss']
+  styleUrls: ['./verify-email.component.scss'],
 })
 export class VerifyEmailComponent implements OnInit {
   token: string = '';
-  formData:any;
+  formData: any;
   isSaving: boolean = false;
+
   constructor(
     public authService: AuthService,
     private readonly route: ActivatedRoute,
     private readonly toastr: ToastrService,
     private readonly auth: AngularFireAuth,
     private readonly emailService: EmailService,
-    private readonly fns: AngularFireFunctions
-  ) { }
+    private readonly fns: AngularFireFunctions,
+    private readonly http: HttpClient
+  ) {}
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       if (params['token']) {
         this.token = params['token'];
         localStorage.setItem('payflexToken', this.token);
         let data = localStorage.getItem('formData');
-        this.formData = JSON.parse(data)
+        this.formData = JSON.parse(data);
         console.log('storedData', JSON.parse(data));
-
       }
     });
-   
   }
 
-  verifyAndCreate(){
+  verifyAndCreate() {
     this.signup(this.formData);
   }
 
-
-  async signup(formData:any) {
+  /* async signup(formData: any) {
     this.isSaving = true;
     try {
-      const userCredential = await this.auth.createUserWithEmailAndPassword(formData.userEmail, formData.password);
+      const userCredential = await this.auth.createUserWithEmailAndPassword(
+        formData.userEmail,
+        formData.password
+      );
       const user = userCredential.user;
-  
+
       if (user) {
         await user.updateProfile({
-          displayName: formData.firstName
+          displayName: formData.firstName,
         });
 
-        this.SendVerificationMail(user);
-  
-        /* const generateLinkFn = this.fns.httpsCallable('generateEmailVerificationLink');
-        const response = await generateLinkFn({ email: formData.userEmail }).toPromise();
-
-        const confirmationLink = response?.link;
-
-        this.emailService.sendConfirmationEmail(formData.userEmail, confirmationLink, formData.firstName).subscribe({
-          next: () => {
-            this.authService.SetUserData(user, this.formData);
-            this.toastr.success('Confirmation email sent!');
-            this.isSaving = false;
-          },
-          error: (err) => {
-            this.toastr.error('Error sending email:', err);
-            this.isSaving = false;
-          }
-        }); */
+        //this.SendVerificationMail(user);
+        this.emailService
+          .sendConfirmationEmail(formData.userEmail, formData.firstName)
+          .subscribe({
+            next: () => {
+              this.toastr.success('Confirmation email sent!');
+              this.authService.SetUserData(user, formData);
+            },
+            error: (error) => {
+              this.toastr.error('Failed to send confirmation email.');
+              console.error('Email sending error:', error);
+            },
+            complete: () => {
+              this.isSaving = false;
+            },
+          });
       }
     } catch (error) {
       this.isSaving = false;
+      this.toastr.error('An error occured, please contact admin');
+      console.error('Signup error:', error);
+    }
+  } */
+
+  async signup(formData: any) {
+    this.isSaving = true;
+    try {
+      const userCredential = await this.auth.createUserWithEmailAndPassword(
+        formData.userEmail,
+        formData.password
+      );
+      const user = userCredential.user;
+
+      if (user) {
+        await user.updateProfile({
+          displayName: formData.firstName,
+        });
+
+        this.emailService
+          .sendConfirmationEmail(formData.userEmail, formData.firstName)
+          .subscribe({
+            next: () => {
+              this.toastr.success('Confirmation email sent!');
+              this.authService.SetUserData(user, formData);
+            },
+            error: (error) => {
+              this.toastr.error('Failed to send confirmation email.');
+              console.error('Email sending error:', error);
+            },
+            complete: () => {
+              this.isSaving = false;
+            },
+          });
+      }
+    } catch (error) {
+      this.isSaving = false;
+      this.toastr.error('An error occurred, please contact admin.');
       console.error('Signup error:', error);
     }
   }
@@ -86,7 +125,9 @@ export class VerifyEmailComponent implements OnInit {
             this.authService.SetUserData(user, this.formData);
           })
           .catch((error: any) => {
-            this.toastr.error('Failed to send confirmation email. Please try again later.');
+            this.toastr.error(
+              'Failed to send confirmation email. Please try again later.'
+            );
             console.error('Error sending email verification:', error);
           });
       })
@@ -98,5 +139,4 @@ export class VerifyEmailComponent implements OnInit {
         this.isSaving = false;
       });
   }
-  
 }
--- src/app/shared/services/email.service.ts
+++ src/app/shared/services/email.service.ts
@@ -2,196 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailService {
-  private readonly firebaseFunctionUrl = 'https://us-central1-hungr-firebase.cloudfunctions.net/sendBrevoEmail'; // Replace with actual URL
+  private readonly firebaseFunctionUrl =
+    'https://us-central1-hungr-firebase.cloudfunctions.net/sendBrevoEmail';
+  private readonly lambdaUrl =
+    'https://2xajnvt3eg.execute-api.us-east-1.amazonaws.com/default/sendEmail';
 
   constructor(private readonly http: HttpClient) {}
 
-  sendConfirmationEmail(email: string, confirmationLink: string, name: string) {
-    
+  /*  sendConfirmationEmail(email: string, name: string) {
     return this.http.post(this.firebaseFunctionUrl, {
       email,
-      subject: "Confirm Your Email",
-      message: `<html>
+      name,
+    });
+  } */
 
-<head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-</head>
-
-<body>
-    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="600"
-        align="center">
-        <tr>
-            <td style="height:15px;"></td>
-        </tr>
-        <tr>
-            <td>
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;"
-                    width="600" align="center">
-                    <tr>
-                        <td style="width: 30px;"></td>
-                        <td style="text-align: left;">
-                            <a href="" target="_blank"><img src="https://anomalytest.co.za/hungr/resetpassword/logo.png"
-                                    alt="" width="102" style="width: 102px;" /></a>
-                        </td>
-                        <td style="text-align: right;">
-                            <a href="" target="_blank"
-                                style="color: #444444;font-family: 'Barlow', sans-serif;font-weight: 500; font-size: 12px;line-height: 12px; margin: 0; ">View
-                                in browser</a>
-                        </td>
-                        <td style="width: 30px;"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td style="height:15px;"></td>
-        </tr>
-        <tr>
-            <td>
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;"
-                    width="600" align="center">
-                    <tr>
-                        <td style="width: 30px;"></td>
-                        <td>
-                            <img src="https://anomalytest.co.za/hungr/confirm/header1.png" alt="" width="540"
-                                style="width: 540px;" />
-                        </td>
-                        <td style="width: 30px;"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td style="height:40px;"></td>
-        </tr>
-        <tr>
-            <td>
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;"
-                    width="600" align="center">
-                    <tr>
-                        <td style="width: 100px;"></td>
-                        <td>
-                            <p
-                                style="text-align:center;color: #444444;font-family: 'Barlow', sans-serif;font-weight: 500; font-size: 14px;line-height: 12px; margin: 0; ">
-                                Hi ${name}<br /><br /><br />
-                                Thank you for signing up for HUNGR.<br /><br /><br />
-                                <b>Please verify your email address</b> by clicking on the button below.
-                            </p>
-                        </td>
-                        <td style="width: 100px;"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td style="height:30px;"></td>
-        </tr>
-        <tr>
-            <td style="text-align: center;">
-                <a href="${confirmationLink}" target="_blank"> <img src="https://anomalytest.co.za/hungr/confirm/button1.png"
-                        alt="" width="150" style="width: 150px;" /></a>
-            </td>
-        </tr>
-        <tr>
-            <td style="height:30px;"></td>
-        </tr>
-        <tr>
-            <td>
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;"
-                    width="600" align="center">
-                    <tr>
-                        <td style="width: 100px;"></td>
-                        <td>
-                            <p
-                                style="text-align:center;color: #444444;font-family: 'Barlow', sans-serif;font-weight: 500; font-size: 14px;line-height: 16px; margin: 0; ">
-                                If you didn't ask to verify this address, you can ignore this email.
-                            </p>
-                        </td>
-                        <td style="width: 100px;"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td style="height:45px;"></td>
-        </tr>
-        <tr>
-            <td>
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;"
-                    width="600" align="center">
-                    <tr>
-                        <td style="width: 130px;"></td>
-                        <td>
-                            <p
-                                style="text-align:center;color: #444444;font-family: 'Barlow', sans-serif;font-weight: 500; font-size: 14px;line-height: 16px; margin: 0; ">
-                                Thanks,<br /><br /><br />
-                                The Hungr Team
-                            </p>
-                        </td>
-                        <td style="width: 130px;"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td style="height:40px;"></td>
-        </tr>
-        <tr>
-            <td style="background-color: rgba(242, 243, 244, 1);">
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;"
-                    width="600" align="center">
-                    <tr>
-                        <td style="height:30px;"></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <table border="0" cellpadding="0" cellspacing="0" role="presentation"
-                                style="vertical-align:middle;" align="center">
-                                <tr>
-                                    <td style="width: 266px;"></td>
-                                    <td><a href="" target="_blank"><img
-                                                src="https://anomalytest.co.za/hungr/resetpassword/insta.png" alt=""
-                                                width="15" style="width: 15px;" /></a></td>
-                                    <td style="width: 15px;"></td>
-                                    <td><a href="" target="_blank"><img
-                                                src="https://anomalytest.co.za/hungr/resetpassword/fb.png" alt=""
-                                                width="7" style="width: 7px;" /></a></td>
-                                    <td style="width: 15px;"></td>
-                                    <td><a href="" target="_blank"><img
-                                                src="https://anomalytest.co.za/hungr/resetpassword/web.png" alt=""
-                                                width="15" style="width: 15px;" /></a></td>
-                                    <td style="width: 266px;"></td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="height:30px;"></td>
-                    </tr>
-                    <tr>
-                        <td style="text-align: center;">
-                            <a href="" target="_blank"
-                                style="color: #444444;font-family: 'Barlow', sans-serif;font-weight: 500; font-size: 12px;line-height: 12px; margin: 0; ">Need
-                                help? Get in touch</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="height:30px;"></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-</body>
-
-</html>`,
+  sendConfirmationEmail(email: string, name: string) {
+    console.log('test');
+    return this.http.post(this.lambdaUrl, {
+      email,
+      name,
     });
   }
 }
